869fd0d9fa6a1d26e1969a29bdbbd54681d24438
BIN LIN <linbinmr@gmail.com>
2017-09-05 14:55:10 -0700

    add test folder for sqlg-trafodion

---
+++ sqlg-trafodion-parent/sqlg-trafodion/src/test/java/org/umlg/sqlg/test/TrafodionAllTest.java
@@ -0,0 +1,10 @@
+package org.umlg.sqlg.test;
+
+import org.umlg.sqlg.AllTest;
+
+/**
+ Created by Bin Lin on 9/4/17.
+ */
+
+public class TrafodionAllTest extends AllTest {
+}
---
+++ sqlg-trafodion-parent/sqlg-trafodion/src/test/java/org/umlg/sqlg/test/TrafodionAnyTest.java
@@ -0,0 +1,11 @@
+package org.umlg.sqlg.test;
+
+import org.umlg.sqlg.AnyTest;
+
+/**
+ * Created by Bin Lin on 9/4/17.
+ */
+
+public class TrafodionAnyTest extends AnyTest {
+
+}
